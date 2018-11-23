9e1aaac7f8c428a115e551f27c7436dc92b4201e
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2018-11-23 10:34:24 -0600

    Update vital_ha.sh

--- vital_ha.sh
+++ vital_ha.sh
@@ -277,10 +277,10 @@ echo -e "*    Create resource for the use of MariaDB in Master      *"
 echo -e "************************************************************"
 pcs resource create mysql ocf:heartbeat:mysql binary="/usr/bin/mysqld_safe" config="/etc/my.cnf" datadir="/mnt/mysql/data" pid="/var/lib/mysql/mysql.pid" socket="/var/lib/mysql/mysql.sock" additional_parameters="--bind-address=0.0.0.0" op start timeout=60s op stop timeout=60s op monitor interval=20s timeout=30s on-fail=standby 
 pcs cluster cib fs_cfg
-pcs cluster cib-push fs_cfg 
+pcs cluster cib-push fs_cfg --config
 pcs -f fs_cfg constraint colocation add mysql with virtual_ip INFINITY
 pcs -f fs_cfg constraint order DrbdFS then mysql
-#pcs cluster cib-push fs_cfg
+pcs cluster cib-push fs_cfg --config
 echo -e "*** Done ***"
 
 echo -e "************************************************************"
@@ -368,7 +368,7 @@ ssh root@$ip_slave "systemctl stop vpbx-monitor"
 ssh root@$ip_slave "systemctl disable vpbx-monitor"
 pcs resource create vpbx-monitor service:vpbx-monitor op monitor interval=30s
 pcs cluster cib fs_cfg
-#pcs cluster cib-push fs_cfg 
+pcs cluster cib-push fs_cfg --config
 pcs -f fs_cfg constraint colocation add vpbx-monitor with virtual_ip INFINITY
 pcs -f fs_cfg constraint order asterisk then vpbx-monitor
 pcs cluster cib fs_cfg
@@ -384,7 +384,7 @@ ssh root@$ip_slave "systemctl stop fail2ban"
 ssh root@$ip_slave "systemctl disable fail2ban"
 pcs resource create fail2ban service:fail2ban op monitor interval=30s
 pcs cluster cib fs_cfg
-#pcs cluster cib-push fs_cfg
+pcs cluster cib-push fs_cfg --config
 pcs -f fs_cfg constraint colocation add fail2ban with virtual_ip INFINITY
 pcs -f fs_cfg constraint order vpbx-monitor then fail2ban
 pcs cluster cib fs_cfg
